48ffac2dd47d2924853d06fa8b4e8c917e90910e
蛤蛤 <proimg@sina.com>
2017-01-28 15:08:22 +0800

    Add files via upload

--- arr.js
+++ arr.js
@@ -1,8 +1,8 @@
-var pic = [];
+﻿var pic = [];
 for (var i=0; i<12; i++)
 {
     pic.push('./img/'+i+'.jpg');
 }
 var backback = ['./img/back0.jpg','./img/back1.jpg','./img/back2.jpg','./img/back3.jpg'];
-var madia = ['bgm','anime','風よ 0074','戦争'];
+var madia = ['bgm音楽','アニメｐｖ','風よ 0074','鹦鹉のvideo'];
 var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4'];
