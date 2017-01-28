e1bee0c609af2afe88f6515b81d5b9d830fc5782
蛤蛤 <proimg@sina.com>
2017-01-28 21:45:00 +0800

    Add files via upload

--- arr.js
+++ arr.js
@@ -4,5 +4,5 @@ for (var i=0; i<12; i++)
     pic.push('./img/'+i+'.jpg');
 }
 var backback = ['./img/back0.jpg','./img/back1.jpg','./img/back2.jpg','./img/back3.jpg'];
-var madia = ['bgm音楽','アニメｐｖ','風よ 0074','鹦鹉のvideo'];
-var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4'];
+var madia = ['bgm音楽','アニメｐｖ','風よ 0074','鹦鹉のvideo','防人の詩'];
+var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4','./img/4.mp3'];
