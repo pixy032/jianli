02ac87748809264ba2587627ce6cae8001f8878e
蛤蛤 <proimg@sina.com>
2017-02-10 03:11:53 +0800

    Update arr.js

--- arr.js
+++ arr.js
@@ -5,5 +5,5 @@ for (var i=0; i<12; i++)
 }
 var kpArr = ['./toshiba/kanpan0.jpg','./toshiba/kanpan1.jpg','./toshiba/kanpan2.jpg','./toshiba/kanpan3.jpg','./toshiba/kanpan4.jpg'];
 var backback = ['./img/back0.jpg','./img/back1.jpg','./img/back2.jpg','./img/back3.jpg'];
-var madia = ['bgm音楽','アニメｐｖ','風よ 0074','インコのvideo','防人の詩'];
-var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4','./img/4.mp3'];
+var madia = ['bgm音楽','アニメｐｖ','風よ 0074','インコのvideo','防人の詩','P.O.W'];
+var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4','./img/4.mp3','./img/5.mp3'];
