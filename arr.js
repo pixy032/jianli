1565cba53129ee573dd48280ae8fdd8c32765cff
蛤蛤 <proimg@sina.com>
2017-02-12 13:12:07 +0800

    Update arr.js

--- arr.js
+++ arr.js
@@ -3,7 +3,7 @@ for (var i=0; i<12; i++)
 {
     pic.push('./img/'+i+'.jpg');
 }
-var kpArr = ['./toshiba/kanpan0.jpg','./toshiba/kanpan1.jpg','./toshiba/kanpan2.jpg','./toshiba/kanpan3.jpg','./toshiba/kanpan4.jpg'];
+var kpArr = ['./img/r1.png','./img/r2.png','./img/p1.png','./img/p2.png','./img/f1.png','./img/f2.png','./img/e1.png','./img/e2.png','./img/d1.png','./img/d2.png','./toshiba/kanpan0.jpg','./toshiba/kanpan1.jpg','./toshiba/kanpan2.jpg','./toshiba/kanpan3.jpg','./toshiba/kanpan4.jpg'];
 var backback = ['./img/back0.jpg','./img/back1.jpg','./img/back2.jpg','./img/back3.jpg'];
 var madia = ['bgm音楽','アニメｐｖ','風よ 0074','インコのvideo','防人の詩','P.O.W'];
 var mSrc = ['./img/0.mp3','./img/1.mp4','./img/2.mp3','./img/3.mp4','./img/4.mp3','./img/5.mp3'];
